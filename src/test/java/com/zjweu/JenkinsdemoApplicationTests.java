95114306634f943c7822a803352c9364c9cab7e9
FourLeafClovers <1710712887@qq.com>
2020-06-30 22:54:39 +0800

    111111

---
+++ src/test/java/com/zjweu/JenkinsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjweu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
